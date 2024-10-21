71af7dc2f4443bae71a8e2699e375d8cad9b30bc
Rohan Wagle <wagle@cs.unc.edu>
2024-10-21 19:18:05 -0400

    add pid and tid to events context

--- lttng_setup.sh
+++ lttng_setup.sh
@@ -3,5 +3,6 @@ lttng enable-channel --kernel kchan
 lttng enable-channel --userspace uchan
 lttng enable-event --kernel --channel=kchan sched_switch,sched_wakeup
 lttng enable-event --userspace --channel=uchan unc_ros*
+lttng add-context -u -e unc_ros_graph_generator:* -t pid -t tid
 lttng list $1
 
